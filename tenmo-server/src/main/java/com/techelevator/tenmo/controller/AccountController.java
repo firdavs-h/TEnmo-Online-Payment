5de725d60a80dfb216f44cb8a6aca1ae62897dec
Olga Plaksina <olgaplaksina@yahoo.com>
2020-11-04 16:14:44 -0500

    capstone

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,8 +1,15 @@
 package com.techelevator.tenmo.controller;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+	
+	
+	public void getBALANCE(Principal p) {
+		
+	}
 
 }
